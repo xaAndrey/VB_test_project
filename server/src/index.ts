4f686906ee72779efbee5043664aa57d83f8699f
andreypisarev <xaandreynx@gmail.com>
2024-06-24 01:07:31 +0300

    [feat]<server>: setting up cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,6 +10,16 @@ app.get('/', (req, res) => {
     res.send('Hello world');
 })
 
+app.all('*', function (req, res, next) {
+    res.header ("Access-Control-Allow-Origin", req.headers.origin); // Переход от исходного * к источнику текущего запроса
+    res.header("Access-Control-Allow-Headers", "X-Requested-With,Content-Type,token");
+    res.header("Access-Control-Allow-Methods", "PUT,POST,PATCH,GET,DELETE,OPTIONS");
+    res.header("Content-Type", "application/json;charset=utf-8");
+    res.header ("Access-Control-Allow-Credentials", 'true'); // Разрешить отправку файлов cookie
+    next();
+});
+
+
 app.use('/api/comments', commentRouter);
 
 app.listen(port, () => {
