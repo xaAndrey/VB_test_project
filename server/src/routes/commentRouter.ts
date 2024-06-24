a0b2f2e6843b727fc5ac1c105c919e1316976f23
andreypisarev <xaandreynx@gmail.com>
2024-06-24 23:41:34 +0300

    [feat]<server>: add request comment by id

--- server/src/routes/commentRouter.ts
+++ server/src/routes/commentRouter.ts
@@ -1,11 +1,19 @@
-import { Router } from "express";
+import { Router, response } from "express";
 
-// /api/documents
+// /api/comments
 const commentRouter = Router();
 
-// /api/documents
+// /api/comments
 commentRouter.get('/', async (req, res) => {
     await fetch('https://jsonplaceholder.typicode.com/comments')
+        .then((response) => response.json())
+        .then((json) => res.json(json));
+})
+
+// /api/comments/:id
+commentRouter.get('/:id', async (req, res) => {
+    const id = req.params.id;
+    await fetch(`https://jsonplaceholder.typicode.com/comments/${id}`)
         .then((response) => response.json())
         .then((json) => res.json(json));
 })
