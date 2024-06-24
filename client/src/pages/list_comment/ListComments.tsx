8ac1eb3fb5eda4f696c13a2a1d86e0fe84c30da1
andreypisarev <xaandreynx@gmail.com>
2024-06-24 17:15:54 +0300

    [feat]<client>: output the data in table format

--- client/src/pages/list_comment/ListComments.tsx
+++ client/src/pages/list_comment/ListComments.tsx
@@ -2,13 +2,15 @@ import { useEffect, useState } from "react";
 import { CommentDto } from "../../api/comments/dto";
 import { fetchAllComments } from "../../api/comments/request";
 import {
+  EuiBasicTable,
+  EuiBasicTableColumn,
+  EuiTableFieldDataColumnType,
   EuiText,
-} from '@elastic/eui';
+} from "@elastic/eui";
 
 export const ListComments = (): JSX.Element => {
   const [comments, setComments] = useState<CommentDto[]>([]);
-  const [variant, setVariant] = useState<string>("json");
-
+  const [variant, setVariant] = useState<string>("table");
 
   useEffect(() => {
     fetchAllComments()
@@ -23,26 +25,85 @@ export const ListComments = (): JSX.Element => {
     console.log(comments);
   }, [comments]);
 
-  return <>
-  <h1>Comments</h1>
-  {variant === "json" && <>
-  <EuiText>
-    <h2>JSON</h2>
-    {comments.map((comment) => {
-      return(
+  const columns: Array<EuiBasicTableColumn<CommentDto>> = [
+    {
+      field: "id",
+      name: "ID",
+    },
+    {
+      field: "email",
+      name: "Email",
+    },
+    {
+      field: "name",
+      name: "Name",
+    },
+  ];
+
+  const getRowsProps = (comment: CommentDto) => {
+    const { id } = comment;
+    return {
+      "data-test-subj": `row-${id}`,
+      className: "customRowClass",
+      onClick: () => {},
+    };
+  };
+
+  const getCellProps = (
+    comment: CommentDto,
+    column: EuiTableFieldDataColumnType<CommentDto>
+  ) => {
+    const { id } = comment;
+    const { field } = column;
+    return {
+      className: "customCellClass",
+      "data-test-subj": `cell-${id}-${String(field)}`,
+      textOnly: true,
+    };
+  };
+
+  return (
+    <>
+      <h1>Comments</h1>
+      {variant === "json" && (
         <>
-        <p>
-        {'{'}<br/>
-        &emsp;{`"id" : ${comment.id},`}<br/>
-        &emsp;{`"name" : ${comment.name},`}<br/>
-        &emsp;{`"email" : ${comment.email},`}<br/>
-        &emsp;{`"body" : ${comment.body}`}<br/>
-        {'},'}
-        </p>
+          <EuiText>
+            <h2>JSON</h2>
+            {comments.map((comment) => {
+              return (
+                <>
+                  <p>
+                    {"{"}
+                    <br />
+                    &emsp;{`"id" : ${comment.id},`}
+                    <br />
+                    &emsp;{`"name" : ${comment.name},`}
+                    <br />
+                    &emsp;{`"email" : ${comment.email},`}
+                    <br />
+                    &emsp;{`"body" : ${comment.body}`}
+                    <br />
+                    {"},"}
+                  </p>
+                </>
+              );
+            })}
+          </EuiText>
         </>
-      );
-    })}
-    </EuiText>
-  </>}
-  </>;
+      )}
+      {variant === "table" && (
+        <>
+          <EuiBasicTable
+            tableCaption="Comments List"
+            items={comments}
+            rowHeader="id"
+            columns={columns}
+            rowProps={getRowsProps}
+            cellProps={getCellProps}
+            responsiveBreakpoint={false}
+          />
+        </>
+      )}
+    </>
+  );
 };
