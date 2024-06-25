ff67ffd8f96a9d2322b070f0f2be643fbc65607e
andreypisarev <xaandreynx@gmail.com>
2024-06-25 20:58:34 +0300

    [feat]<client>: add loader on Comment Page

--- client/src/pages/comment/Comment.tsx
+++ client/src/pages/comment/Comment.tsx
@@ -2,31 +2,73 @@ import { useEffect, useState } from "react";
 import { CommentDto } from "../../api/comments/dto";
 import { fetchCommentById } from "../../api/comments/request";
 import { useNavigate, useParams } from "react-router-dom";
+import { Box, CircularProgress } from "@mui/material";
 
 export const Comment = () => {
   const [comment, setComment] = useState<CommentDto>();
+  const [loading, setLoading] = useState<boolean>(true);
+
   const { id } = useParams();
   const navigate = useNavigate();
 
-  useEffect(() => id === undefined ? console.log("undefined") : console.log(parseInt(id)), [id]);
-  useEffect(() => console.log(comment), [comment]);
+  useEffect(
+    () =>
+      id === undefined ? console.log("undefined") : console.log(parseInt(id)),
+    [id]
+  );
+  useEffect(() => {
+    console.log(comment);
+    if (comment !== undefined) {
+      setLoading(false);
+    }
+  }, [comment]);
 
   useEffect(() => {
-    if (id === undefined) {throw new Error("Id id undefined!")}
+    if (id === undefined) {
+      throw new Error("Id is undefined!");
+    }
     fetchCommentById(parseInt(id))
-    .then((response) => {
+      .then((response) => {
         if (response.ok) return response.json();
-    })
-    .then((json) => setComment(json))
-    .catch((error) => console.error(error));
+      })
+      .then((json) => {
+        setComment(json);
+      })
+      .catch((error) => console.error(error));
   }, []);
 
-  return <>
-  {comment?.id === undefined && <h1>Комментарий с таким ID не существует!</h1>}
+  return (
+    <>
+      {loading && (
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          height="100vh"
+        >
+          <CircularProgress />
+        </Box>
+      )}
+      {comment?.id === undefined && !loading && (
+        <h1>Комментарий с таким ID не существует!</h1>
+      )}
 
-  <h1 onClick={() => navigate(`/?id=${comment?.id}`)}>{comment?.id}</h1>
-  <h2 onClick={() => navigate(`/?name=${comment?.name}`)}>{comment?.name}</h2>
-  <h3 onClick={() => navigate(`/?email=${comment?.email}`)}>{comment?.email}</h3>
-  <h4 onClick={() => navigate(`/?id=${comment?.id}&name=${comment?.name}&email=${comment?.email}`)}>{comment?.body}</h4>
-  </>;
+      <h1 onClick={() => navigate(`/?id=${comment?.id}`)}>{comment?.id}</h1>
+      <h2 onClick={() => navigate(`/?name=${comment?.name}`)}>
+        {comment?.name}
+      </h2>
+      <h3 onClick={() => navigate(`/?email=${comment?.email}`)}>
+        {comment?.email}
+      </h3>
+      <h4
+        onClick={() =>
+          navigate(
+            `/?id=${comment?.id}&name=${comment?.name}&email=${comment?.email}`
+          )
+        }
+      >
+        {comment?.body}
+      </h4>
+    </>
+  );
 };
